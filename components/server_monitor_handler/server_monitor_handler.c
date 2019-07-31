ea0e3e6778d4e492130ec2c6ff896f3e7c3b310e
drachezoil <zoilo.abad@laposte.net>
2019-07-31 14:09:34 +1200

    Spiffs webserver + config.xml

--- components/server_monitor_handler/server_monitor_handler.c
+++ components/server_monitor_handler/server_monitor_handler.c
@@ -16,73 +16,253 @@
 #include "esp_log.h"
 
 #include "esp_vfs.h"
+#include "esp_spiffs.h"
 #include "esp_http_server.h"
+
+/* Max length a file path can have on storage */
+#define FILE_PATH_MAX (ESP_VFS_PATH_MAX + CONFIG_SPIFFS_OBJ_NAME_LEN)
+
+/* Max size of an individual file. Make sure this
+ * value is same as that set in upload_script.html */
+#define MAX_FILE_SIZE   (200*1024) // 200 KB
+#define MAX_FILE_SIZE_STR "200KB"
+
+/* Scratch buffer size */
+#define SCRATCH_BUFSIZE  8192
+
+struct file_server_data {
+    /* Base path of file storage */
+    char base_path[ESP_VFS_PATH_MAX + 1];
+
+    /* Scratch buffer for temporary storage during file transfer */
+    char scratch[SCRATCH_BUFSIZE];
+};
 
 static const char *TAG = "server_monitor_handler";
 
-/* If adding or changing and site files, make sure they added or removed from component.mk file. 
- * Also, if changes are made you need to clean then rebuild for it to compile those new files.
- * It does not do it automatically. 
- */
-extern const uint8_t index_html_start[] asm("_binary_index_html_start");
-extern const uint8_t index_html_end[]   asm("_binary_index_html_end");
+#define IS_FILE_EXT(filename, ext) \
+    (strcasecmp(&filename[strlen(filename) - sizeof(ext) + 1], ext) == 0)
 
-extern const uint8_t style_css_start[] asm("_binary_style_css_start");
-extern const uint8_t style_css_end[]   asm("_binary_style_css_end");
-
-extern const uint8_t log_html_start[] asm("_binary_log_html_start");
-extern const uint8_t log_html_end[]   asm("_binary_log_html_end");
-
-extern const uint8_t sensor_data_html_start[] asm("_binary_sensor_data_html_start");
-extern const uint8_t sensor_data_html_end[]   asm("_binary_sensor_data_html_end");
-
-/* Handler GET request for /index.html */
-esp_err_t index_html_get_handler(httpd_req_t *req)
+/* Set HTTP response content type according to file extension */
+static esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filename)
 {
-	ESP_LOGD(TAG, "index.html requested");
-	httpd_resp_set_type(req,"text/html");
-    httpd_resp_send(req, (const char *)index_html_start, (index_html_end-1) - index_html_start);
-	/* Send empty chunk to signal HTTP response completion */
-    httpd_resp_sendstr_chunk(req, NULL);
-	return ESP_OK;
+    if (IS_FILE_EXT(filename, ".pdf")) {
+        return httpd_resp_set_type(req, "application/pdf");
+    } else if (IS_FILE_EXT(filename, ".html")) {
+        return httpd_resp_set_type(req, "text/html");
+    } else if (IS_FILE_EXT(filename, ".jpeg")) {
+        return httpd_resp_set_type(req, "image/jpeg");
+    } else if (IS_FILE_EXT(filename, ".ico")) {
+        return httpd_resp_set_type(req, "image/x-icon");
+    }else if (IS_FILE_EXT(filename, ".xml")) {
+        return httpd_resp_set_type(req, "text/xml");
+    }else if (IS_FILE_EXT(filename, ".css")) {
+        return httpd_resp_set_type(req, "text/css");
+    }
+    /* This is a limited set only */
+    /* For any other type always set as plain text */
+    return httpd_resp_set_type(req, "text/plain");
 }
 
-/* Handler GET request for /log.html */
-esp_err_t log_html_get_handler(httpd_req_t *req)
+
+/* Copies the full path into destination buffer and returns
+ * pointer to path (skipping the preceding base path) */
+static const char* get_path_from_uri(char *dest, const char *base_path, const char *uri, size_t destsize)
 {
-	ESP_LOGD(TAG, "log.html requested");
-	httpd_resp_set_type(req,"text/html");
-    httpd_resp_send(req, (const char *)log_html_start, (log_html_end-1) - log_html_start);
-	/* Send empty chunk to signal HTTP response completion */
-    httpd_resp_sendstr_chunk(req, NULL);
-	return ESP_OK;
+    const size_t base_pathlen = strlen(base_path);
+    size_t pathlen = strlen(uri);
+
+    const char *quest = strchr(uri, '?');
+    if (quest) {
+        pathlen = MIN(pathlen, quest - uri);
+    }
+    const char *hash = strchr(uri, '#');
+    if (hash) {
+        pathlen = MIN(pathlen, hash - uri);
+    }
+
+    if (base_pathlen + pathlen + 1 > destsize) {
+        /* Full path string won't fit into destination buffer */
+        return NULL;
+    }
+
+    /* Construct full path (base + path) */
+    strcpy(dest, base_path);
+    strlcpy(dest + base_pathlen, uri, pathlen + 1);
+
+    /* Return pointer to path, skipping the base */
+    return dest + base_pathlen;
 }
 
-/* Handler GET request for /sensor_data.html */
-esp_err_t sensor_data_html_get_handler(httpd_req_t *req)
+/* Handler to download a file kept on the server */
+static esp_err_t download_get_handler(httpd_req_t *req)
 {
-	ESP_LOGD(TAG, "sensor_data.html requested");
-	httpd_resp_set_type(req,"text/html");
-    httpd_resp_send(req, (const char *)sensor_data_html_start, (sensor_data_html_end-1) - sensor_data_html_start);
-	/* Send empty chunk to signal HTTP response completion */
-    httpd_resp_sendstr_chunk(req, NULL);
-	return ESP_OK;
+    char filepath[FILE_PATH_MAX];
+    FILE *fd = NULL;
+    struct stat file_stat;
+	
+    const char *filename = get_path_from_uri(filepath, ((struct file_server_data *)req->user_ctx)->base_path, req->uri, sizeof(filepath));
+    if (!filename) {
+        ESP_LOGE(TAG, "Filename is too long");
+        /* Respond with 500 Internal Server Error */
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Filename too long");
+        return ESP_FAIL;
+    }
+
+    if (stat(filepath, &file_stat) == -1) {
+        /* If file not present on SPIFFS check if URI
+         * corresponds to one of the hardcoded paths */
+		if (strcmp(filename, "/") == 0) {
+            filename = "index.html"; 
+            strcpy(filepath,"/spiffs/index.html"); 
+		}
+		else{
+			ESP_LOGE(TAG, "Failed to stat file : %s", filepath);
+			/* Respond with 404 Not Found */
+			httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "File does not exist");
+			return ESP_FAIL;
+		}
+    }
+
+    fd = fopen(filepath, "r");
+    if (!fd) {
+        ESP_LOGE(TAG, "Failed to read existing file : %s", filepath);
+        /* Respond with 500 Internal Server Error */
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to read existing file");
+        return ESP_FAIL;
+    }
+
+    ESP_LOGI(TAG, "Sending file : %s (%ld bytes)...", filename, file_stat.st_size);
+    set_content_type_from_file(req, filename);
+
+    /* Retrieve the pointer to scratch buffer for temporary storage */
+    char *chunk = ((struct file_server_data *)req->user_ctx)->scratch;
+    size_t chunksize;
+    do {
+        /* Read file in chunks into the scratch buffer */
+        chunksize = fread(chunk, 1, SCRATCH_BUFSIZE, fd);
+
+        /* Send the buffer contents as HTTP response chunk */
+        if (httpd_resp_send_chunk(req, chunk, chunksize) != ESP_OK) {
+            fclose(fd);
+            ESP_LOGE(TAG, "File sending failed!");
+            /* Abort sending file */
+            httpd_resp_sendstr_chunk(req, NULL);
+            /* Respond with 500 Internal Server Error */
+            httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to send file");
+            return ESP_FAIL;
+        }
+
+        /* Keep looping till the whole file is sent */
+    } while (chunksize != 0);
+
+    /* Close file after sending complete */
+    fclose(fd);
+    ESP_LOGI(TAG, "File sending complete");
+
+    /* Respond with an empty chunk to signal HTTP response completion */
+    httpd_resp_send_chunk(req, NULL, 0);
+    return ESP_OK;
 }
 
-/* Handler GET request for /style.css */
-esp_err_t style_css_get_handler(httpd_req_t *req)
+/* Handler to upload a file onto the server */
+static esp_err_t upload_post_handler(httpd_req_t *req)
 {
-	ESP_LOGD(TAG, "style.css requested");
-	httpd_resp_set_type(req,"text/css");
-    httpd_resp_send(req, (const char *)style_css_start, (style_css_end-1) - style_css_start);
-	/* Send empty chunk to signal HTTP response completion */
-    httpd_resp_sendstr_chunk(req, NULL);
-	return ESP_OK;
+    char filepath[FILE_PATH_MAX] = "/spiffs/config.xml";
+    FILE *fd = NULL;
+    
+    const char *filename = "config.xml";
+	
+
+    ESP_LOGI(TAG, "Deleting file : %s", filename);
+    /* Delete old file */
+    unlink(filepath);
+
+    fd = fopen(filepath, "w");
+    if (!fd) {
+        ESP_LOGE(TAG, "Failed to create file : %s", filepath);
+        /* Respond with 500 Internal Server Error */
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to create file");
+        return ESP_FAIL;
+    }
+
+    ESP_LOGI(TAG, "Receiving file : %s...", filename);
+
+    /* Retrieve the pointer to scratch buffer for temporary storage */
+    char *buf = ((struct file_server_data *)req->user_ctx)->scratch;
+    int received;
+
+    /* Content length of the request gives
+     * the size of the file being uploaded */
+    int remaining = req->content_len;
+
+    while (remaining > 0) {
+
+        ESP_LOGI(TAG, "Remaining size : %d", remaining);
+        /* Receive the file part by part into a buffer */
+        if ((received = httpd_req_recv(req, buf, MIN(remaining, SCRATCH_BUFSIZE))) <= 0) {
+            if (received == HTTPD_SOCK_ERR_TIMEOUT) {
+                /* Retry if timeout occurred */
+                continue;
+            }
+
+            /* In case of unrecoverable error,
+             * close and delete the unfinished file*/
+            fclose(fd);
+            unlink(filepath);
+
+            ESP_LOGE(TAG, "File reception failed!");
+            /* Respond with 500 Internal Server Error */
+            httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to receive file");
+            return ESP_FAIL;
+        }
+
+        /* Write buffer content to file on storage */
+        if (received && (received != fwrite(buf, 1, received, fd))) {
+            /* Couldn't write everything to file!
+             * Storage may be full? */
+            fclose(fd);
+            unlink(filepath);
+
+            ESP_LOGE(TAG, "File write failed!");
+            /* Respond with 500 Internal Server Error */
+            httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to write file to storage");
+            return ESP_FAIL;
+        }
+
+        /* Keep track of remaining size of
+         * the file left to be uploaded */
+        remaining -= received;
+    }
+
+    /* Close file upon upload completion */
+    fclose(fd);
+    ESP_LOGI(TAG, "File reception complete");
+
+    httpd_resp_sendstr(req, "File uploaded successfully");
+    return ESP_OK;
 }
 
 /* Function to start the file server */
 esp_err_t start_server()
 {
+	const char *base_path = "/spiffs";
+	static struct file_server_data *server_data = NULL;
+	
+    if (server_data) {
+        ESP_LOGE(TAG, "File server already started");
+        return ESP_ERR_INVALID_STATE;
+    }
+	
+	/* Allocate memory for server data */
+    server_data = calloc(1, sizeof(struct file_server_data));
+    if (!server_data) {
+        ESP_LOGE(TAG, "Failed to allocate memory for server data");
+        return ESP_ERR_NO_MEM;
+    }
+    strlcpy(server_data->base_path, base_path,
+            sizeof(server_data->base_path));
+			
     httpd_handle_t server = NULL;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
@@ -97,41 +277,22 @@ esp_err_t start_server()
         return ESP_FAIL;
     }
 	
-	httpd_uri_t index_html = {
+	/* URI handler for getting uploaded files */
+    httpd_uri_t file_download = {
+        .uri       = "/*",  // Match all URIs of type /path/to/file
+        .method    = HTTP_GET,
+        .handler   = download_get_handler,
+        .user_ctx  = server_data    // Pass server data as context
+    };
+    httpd_register_uri_handler(server, &file_download);
+	
+	httpd_uri_t upload_post = {
 		.uri       = "/",
-		.method    = HTTP_GET,
-		.handler   = index_html_get_handler,
-		.user_ctx  = NULL
+		.method    = HTTP_POST,
+		.handler   = upload_post_handler,
+		.user_ctx  = server_data
 	};
-	httpd_uri_t index_html1 = {
-		.uri       = "/index.html",
-		.method    = HTTP_GET,
-		.handler   = index_html_get_handler,
-		.user_ctx  = NULL
-	};
-	httpd_register_uri_handler(server, &index_html);
-	httpd_register_uri_handler(server, &index_html1);
-	httpd_uri_t log_html = {
-		.uri       = "/log.html",
-		.method    = HTTP_GET,
-		.handler   = log_html_get_handler,
-		.user_ctx  = NULL
-	};
-	httpd_register_uri_handler(server, &log_html);
-	httpd_uri_t sensor_data_html = {
-		.uri       = "/sensor_data.html",
-		.method    = HTTP_GET,
-		.handler   = sensor_data_html_get_handler,
-		.user_ctx  = NULL
-	};
-	httpd_register_uri_handler(server, &sensor_data_html);
-	httpd_uri_t style_css = {
-		.uri       = "/style.css",
-		.method    = HTTP_GET,
-		.handler   = style_css_get_handler,
-		.user_ctx  = NULL
-	};
-	httpd_register_uri_handler(server, &style_css);
+	httpd_register_uri_handler(server, &upload_post);
 	
     return ESP_OK;
 }
--- main/main.c
+++ main/main.c
@@ -10,6 +10,7 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "esp_spiffs.h"
 #include "tcpip_adapter.h"
 
 #include "lwip/err.h"
@@ -135,6 +136,40 @@ void init_coms(void){
              ESP_WIFI_SSID, ESP_WIFI_PASS);
 }
 
+/* Function to initialize SPIFFS */
+static esp_err_t init_spiffs(void){
+    ESP_LOGI(TAG, "Initializing SPIFFS");
+
+    esp_vfs_spiffs_conf_t conf = {
+      .base_path = "/spiffs",
+      .partition_label = NULL,
+      .max_files = 5,   // This decides the maximum number of files that can be created on the storage
+      .format_if_mount_failed = true
+    };
+
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        } else if (ret == ESP_ERR_NOT_FOUND) {
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+        return ESP_FAIL;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info(NULL, &total, &used);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+        return ESP_FAIL;
+    }
+
+    ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    return ESP_OK;
+}
+
 /* Declare the function which starts the file server.
  * Implementation of this function is to be found in
  * server.c */
@@ -194,8 +229,10 @@ void app_main(void) {
 	//Coms
 	ESP_LOGD(TAG, "Coms Initialize" );
 	init_coms();
+	
+	/* Start spiffs */
+    init_spiffs();
 
-    /* Start the server */
     ESP_ERROR_CHECK(start_server());
 	
 	//Screen
